5c2957f46c437036e8848f604fdae42725ea2891
w.dehai <342252328@qq.com>
2020-07-31 17:49:40 +0800

    refactor: 移除DataSourceConfig配置

--- mybatis-pro-samples/spring-boot/src/main/java/com/github/dreamroute/mybatis/pro/sample/springboot/config/SqlPrinterConfig.java
+++ mybatis-pro-samples/spring-boot/src/main/java/com/github/dreamroute/mybatis/pro/sample/springboot/config/SqlPrinterConfig.java
@@ -2,24 +2,18 @@ package com.github.dreamroute.mybatis.pro.sample.springboot.config;
 
 import java.util.Properties;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.github.dreamroute.sqlprinter.interceptor.SqlPrinter;
-import com.zaxxer.hikari.HikariDataSource;
 
+/**
+ * @author w.dehai
+ *
+ */
 @Configuration
-public class DataSourceConfig {
-
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public DataSource datasource() {
-        return new HikariDataSource();
-    }
+public class SqlPrinterConfig {
 
     @Value("${sql-show:true}")
     private String show;
