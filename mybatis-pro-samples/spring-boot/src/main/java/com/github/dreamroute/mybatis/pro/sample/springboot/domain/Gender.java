56630df32c30fed23dcedb30d0e2bc13b90205aa
w.dehai <342252328@qq.com>
2021-01-18 16:57:35 +0800

    feat: 研究分页插件原理

--- mybatis-pro-samples/spring-boot/src/main/java/com/github/dreamroute/mybatis/pro/sample/springboot/domain/Gender.java
+++ mybatis-pro-samples/spring-boot/src/main/java/com/github/dreamroute/mybatis/pro/sample/springboot/domain/Gender.java
@@ -1,25 +1,16 @@
 package com.github.dreamroute.mybatis.pro.sample.springboot.domain;
 
 import com.github.dreamroute.mybatis.pro.core.typehandler.EnumMarker;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum  Gender implements EnumMarker {
+
     MALE("男", 1), FEMALE("女", 2);
 
-    private String desc;
-    private Integer value;
+    private final String desc;
+    private final Integer value;
 
-    Gender(String desc, Integer value) {
-        this.desc = desc;
-        this.value = value;
-    }
-
-    @Override
-    public Integer getValue() {
-        return this.value;
-    }
-
-    @Override
-    public String getDesc() {
-        return this.desc;
-    }
 }
