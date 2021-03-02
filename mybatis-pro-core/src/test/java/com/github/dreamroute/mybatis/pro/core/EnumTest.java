a11602b58199d685f3fa42a768bbf28a34fb6918
w.dehai <342252328@qq.com>
2021-03-02 14:59:01 +0800

    fix: 枚举使用lombok

--- mybatis-pro-core/src/test/java/com/github/dreamroute/mybatis/pro/core/EnumTest.java
+++ mybatis-pro-core/src/test/java/com/github/dreamroute/mybatis/pro/core/EnumTest.java
@@ -7,15 +7,16 @@ import org.junit.jupiter.api.Test;
 
 import static com.github.dreamroute.mybatis.pro.core.EnumTest.Gender.FEMALE;
 import static com.github.dreamroute.mybatis.pro.core.EnumTest.Gender.MALE;
-import static lombok.AccessLevel.PRIVATE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnumTest {
 
     @Getter
-    @AllArgsConstructor(access = PRIVATE)
+    @AllArgsConstructor
     enum Gender implements EnumMarker {
-        MALE("男", 1), FEMALE("女", 2);
+
+        MALE("男", 1),
+        FEMALE("女", 2);
 
         private final String desc;
         private final Integer value;
