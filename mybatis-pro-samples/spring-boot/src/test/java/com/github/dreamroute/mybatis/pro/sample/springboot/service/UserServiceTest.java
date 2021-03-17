005561f90fd7ebdf04c87f08690938e88781707c
wangdehai <wangdehai@yzw.cn>
2021-03-17 18:34:21 +0800

    build: 升级sqlprinter

--- mybatis-pro-samples/spring-boot/src/test/java/com/github/dreamroute/mybatis/pro/sample/springboot/service/UserServiceTest.java
+++ mybatis-pro-samples/spring-boot/src/test/java/com/github/dreamroute/mybatis/pro/sample/springboot/service/UserServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * @author w.dehai
  */
 @SpringBootTest
-public class UserServiceTest {
+class UserServiceTest {
 
     @Autowired
     private UserService userService;
