f04093350a9b96230fe07e5feb30ffb4fbdd7e0c
w.dehai <w.dehai>
2021-04-13 15:35:36 +0800

    feat: Id和Ids增加序列化

--- mybatis-pro-service/src/main/java/com/github/dreamroute/mybatis/pro/service/adaptor/id/Ids.java
+++ mybatis-pro-service/src/main/java/com/github/dreamroute/mybatis/pro/service/adaptor/id/Ids.java
@@ -3,13 +3,15 @@ package com.github.dreamroute.mybatis.pro.service.adaptor.id;
 import com.github.dreamroute.mybatis.pro.service.adaptor.validator.ElementNotEmpty;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 前端请求中只有多个id参数
  *
  * @author w.dehai
  */
 @Data
-public class Ids {
+public class Ids implements Serializable {
 
     @ElementNotEmpty
     private Long[] ids;
