623c4ffbacc19a0451decec7ce5a862b004e4758
w.dehai <342242328@qq.com>
2022-02-10 22:25:00 +0800

    doc: 编写注释

--- mybatis-pro-interceptor/src/main/java/com/github/dreamroute/mybatis/pro/interceptor/LimitColumnInterceptor.java
+++ mybatis-pro-interceptor/src/main/java/com/github/dreamroute/mybatis/pro/interceptor/LimitColumnInterceptor.java
@@ -110,6 +110,33 @@ public class LimitColumnInterceptor implements Interceptor, ApplicationListener<
          *         "param2": v2
          *     }
          * </pre>
+         *
+         * 关于sqlsource参考这里：https://www.imooc.com/article/319115
+         * 1. DynamicSqlSource：针对动态 SQL 和 ${} 占位符的 SQL
+         * 2. RawSqlSource：针对 #{}占位符的 SQL
+         * 3. ProviderSqlSource：针对 @*Provider 注解 提供的 SQL
+         * 4. StaticSqlSource：仅包含有 ?占位符的 SQL
+         *
+         * 对于SqlSource的创建，使用的是{@link org.apache.ibatis.scripting.xmltags.XMLScriptBuilder#parseScriptNode}方法，此方法的返回值只有2种，一种是DynamicSqlSource，另一种是RawSqlSource（内部包装了一个StaticSqlSource），
+         * 而对于这两种（其实共有4中，除了这两种还有StaticSqlSource和ProviderSqlSource（不常见））SqlSource，调用getBoundSql得到的BoundSql都是相同的，都是得到了带有问号"?"的sql，所以对于编写插件而言，可以使用BoundSql的getSql()方法，
+         * 只是需要注意将BoundSql的属性补全，用于处理含有in的sql，方法类似如下：
+         * <pre>
+         *     private static void copyProps(BoundSql oldBs, BoundSql newBs, Configuration config) {
+         *         MetaObject oldMo = config.newMetaObject(oldBs);
+         *         Object ap = oldMo.getValue("additionalParameters");
+         *         Object mp = oldMo.getValue("metaParameters");
+         *
+         *         MetaObject newMo = config.newMetaObject(newBs);
+         *         newMo.setValue("additionalParameters", ap);
+         *         newMo.setValue("metaParameters", mp);
+         *     }
+         * </pre>
+         *
+         * 对于mybatis而言：每一次查询都会使用MappedStatement创建一个新的BoundSql，方法在：{@link org.apache.ibatis.executor.CachingExecutor#query}，而MappedStatement不会被改变，因此如果编写插件时如果修改了MappedStatement
+         * 是不行的，会引起并发问题，而对于BoundSql可以随意修改，BoundSql从上述方法被创建之后会一直持续到查询结束，并且在被创建之后的后续执行中，会被写入到StatementHandler中，因此编写插件时，如果拦截的是StatementHandler，那么可以直接
+         * 从StatementHandler中获取原始BoundSql，而对于在插件中需要新建BoundSql的场景，只需要注意把BoundSql中的参数填充满即可
+         *
+         * 事实上：mybatis真是的固定对象是MappedStatement，每次的查询都是从一个MappedStatement开始的，后续的都是新创建的对象，只要不破坏MappedStatement的结构，基本上不会引发并发问题
          */
         if (parameterObject instanceof MapperMethod.ParamMap) {
             MapperMethod.ParamMap<Object> params = (MapperMethod.ParamMap<Object>) parameterObject;
