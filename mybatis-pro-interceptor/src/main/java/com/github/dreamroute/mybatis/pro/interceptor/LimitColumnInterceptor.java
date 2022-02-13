df29861691bd832d25e3264347748f75bb118916
w.dehai <342242328@qq.com>
2022-02-13 21:45:59 +0800

    doc: 编写注释

--- mybatis-pro-interceptor/src/main/java/com/github/dreamroute/mybatis/pro/interceptor/LimitColumnInterceptor.java
+++ mybatis-pro-interceptor/src/main/java/com/github/dreamroute/mybatis/pro/interceptor/LimitColumnInterceptor.java
@@ -136,7 +136,36 @@ public class LimitColumnInterceptor implements Interceptor, ApplicationListener<
          * 是不行的，会引起并发问题，而对于BoundSql可以随意修改，BoundSql从上述方法被创建之后会一直持续到查询结束，并且在被创建之后的后续执行中，会被写入到StatementHandler中，因此编写插件时，如果拦截的是StatementHandler，那么可以直接
          * 从StatementHandler中获取原始BoundSql，而对于在插件中需要新建BoundSql的场景，只需要注意把BoundSql中的参数填充满即可
          *
-         * 事实上：mybatis真是的固定对象是MappedStatement，每次的查询都是从一个MappedStatement开始的，后续的都是新创建的对象，只要不破坏MappedStatement的结构，基本上不会引发并发问题
+         * 事实上：mybatis真是的固定对象是MappedStatement，每次的查询都是从一个MappedStatement开始的，后续的都是新创建的对象，只要不破坏MappedStatement的结构，基本上不会引发并发问题，
+         * 观察mybatis的查询流程，如果不考虑一级缓存的情况，其实真正的查库是从{@link org.apache.ibatis.executor.SimpleExecutor#doQuery}开始的，逻辑是：
+         * 1. 使用configuration创建StatementHandler
+         * 2. 调用prepareStatement方法
+         * 3. StatementHandler调用query方法
+         * 所以，编写插件如果需要新的查询，那么创建一个StatementHandler即可。
+         *
+         * 综上所述，编写插件时，对于新的查询，步骤是：
+         * 1. 获取旧的BoundSql
+         * 2. 创建新的BoundSql
+         *  2.1 对于创建BoundSql有两种方式，一种是直接创建，一种是创建SqlSource（比如StaticSqlSource）然后调用getBoundSql
+         *  2.2. 这里不能直接使用第二种，因为如果是DyanamicSqlSource创建出来的SqlSource是带有AddtionalParameter的，而StaticSqlSouce就没有
+         *  2.3. 如果要使用第二种，依然需要把原来的BoundSql中的AdditionalParameter复制到新的BoundSql中
+         *  2.4. 并且如果使用第二种，在创建StaticSqlSource时需要使用带有parameterMappings参数的构造方法，否则就不用，因为parameterMappings参数终归目的是写入到BoundSql中
+         * 3. 将旧的BoundSql属性复制到新的里面（如果参数不变的话）
+         * 4. 处理BoundSql中的ParameterMappings，如果有变化的话，需要新增
+         * 5. 创建StaticSqlSource
+         * 6. 创建MappedStatement（需要注意ms的id，因为sqlprinter打印sql是根据id来的，注意是否需要与元id相同）
+         *  6.1 特别注意：因为此时的SqlSource是固定的StaticSqlSource，里面的foreach已经被处理过了，问号参数个数已经确定，所以不能缓存ms，必须每次使用新的
+         *  6.2 由于6.1的特性以及插件里面使用了一些反射，所以引入插件势必会造成mybatis性能的下降，这是便捷与性能之间的折中
+         *  6.3 如果使用原始的SqlSource而不使用StaticSqlSource，主要是针对DynamicSqlSource，RawSqlSource不受影响。那么就可以缓存ms。
+         * 7. 获取Executor
+         * 8. 使用configuration构建StatementHandler
+         * 9. 调用prepareStatement方法
+         * 10. StatementHandler调用query方法
+         *
+         * // 带缓存ms的查询
+         * 问题描述：由于上述方法构建的BoundSql中的sql是固定的，而带有foreach的sql语句中的问号个数是动态的，所有就无法缓存ms，原因是采用了固定的StaticSqlSource
+         * 解决方案：在构建ms的时候使用原始的SqlSource，在拼接上需要的sql片段（针对dynamicsqlsource方式，raw方式不存在这个问题）
+         *
          */
         if (parameterObject instanceof MapperMethod.ParamMap) {
             MapperMethod.ParamMap<Object> params = (MapperMethod.ParamMap<Object>) parameterObject;
