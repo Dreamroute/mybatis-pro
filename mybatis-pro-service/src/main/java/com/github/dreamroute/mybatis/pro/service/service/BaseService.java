f3ef02985d35016188901c7af07d5992b4ffde1d
wangdehai <wangdehai@yzw.cn>
2022-11-25 11:18:18 +0800

    feat: 增加注释

--- mybatis-pro-service/src/main/java/com/github/dreamroute/mybatis/pro/service/service/BaseService.java
+++ mybatis-pro-service/src/main/java/com/github/dreamroute/mybatis/pro/service/service/BaseService.java
@@ -8,22 +8,36 @@ import java.util.List;
 public interface BaseService<T, ID> {
 
     /**
-     * 单个新增
+     * 单个新增（<code>null</code>值也会被保存）
+     *
+     * @param entity 需要保存的实体对象
+     * @return 返回保存成功的实体（自增主键会包含在返回值中）
      */
     T insert(T entity);
 
     /**
-     * 单个新增（实体为空的属性不新增到数据库）
+     * 单个新增（<code>null</code>值会被忽略）
+     *
+     * @param entity 需要保存的实体对象
+     * @return 返回保存成功的实体（自增主键会包含在返回值中）
      */
     T insertExcludeNull(T entity);
 
     /**
-     * 批量新增（实体为空的属性也更新到数据库）
+     * 批量新增（<code>null</code>值会被忽略）
+     *
+     * @param entityList 需要保存的实体对象列表
+     * @return 返回保存成功的实体（自增主键会包含在返回值中）
      */
     List<T> insertList(List<T> entityList);
 
+
     /**
-     * 批量新增（实体为空的属性也更新到数据库）
+     * 批量新增（<code>null</code>值会被忽略）
+     *
+     * @param entityList 需要保存的实体对象列表
+     * @param partition entityList会按照没批partition条数，分批次保存，避免数据太大报错
+     * @return 返回保存成功的实体（自增主键会包含在返回值中）
      */
     List<T> insertList(List<T> entityList, int partition);
 
@@ -49,16 +63,27 @@ public interface BaseService<T, ID> {
 
     /**
      * 根据id单个查询
+     *
+     * @param id 主键
+     * @param cols 需要返回的列名数组
+     * @return 返回id对应的数据
      */
     T selectById(ID id, String... cols);
 
     /**
      * 根据id批量查询
+     *
+     * @param ids 主键列表
+     * @param cols 需要返回的列名数组
+     * @return 返回id对应的数据
      */
     List<T> selectByIds(List<ID> ids, String... cols);
 
     /**
      * 全表查询
+     *
+     * @param cols 需要返回的列名数组
+     * @return 返回全部数据
      */
     List<T> selectAll(String... cols);
 
